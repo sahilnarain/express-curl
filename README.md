4896d2cd2565f0dc9736ee202cb5939bdbc34f79
Sahil Narain <sahil.narain@gmail.com>
2017-11-23 14:05:19 +0000

    Init

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # express-curl
-Simple but effective, this express middleware prints out the equivalent curl command for debugging / replication.
+
+Simple but effective, this `ExpressJS` middleware prints out the equivalent curl command. Typically this would be used as a utility for backend developers for replicating requests that may be causing problems.
+
+
+### Installation
+Install the package by running the command below, from the root of your NodeJS project.
+
+```npm install --save express-curl```
+
+
+### Usage
+1. Include the package in your ExpressJS app along with other dependencies.
+
+```
+const fs = require('fs');
+const express = require('express');
+
+// Include express-curl as a dependency
+const expressCurl = require('express-curl);
+
+let app = express();
+...
+```
+
+2. Declare this as a middleware just above the route definitions
+
+```
+app.use(bodyParser.json());
+app.use(multiplart());
+...
+
+// Include the express curl middleware
+app.use(expressCurl);
+
+...
+app.use('/auth', authRouter);
+app.use('/users', usersRouter);
+```
+
+Now the `stdout` will print the curl.
+
+![Alt text](https://i.imgur.com/DHTTrj9.png "curl")
---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const _buildCurl = function (params) {
+  if (!params.url) {
+    console.error('Input missing: URL');
+  }
+
+  if (!params.verb) {
+    console.error('Input missing: HTTP verb');
+  }
+
+  let _headers = '';
+  let _body = '';
+
+  try {
+    if (params.headers) {
+      if (params.headers['content-length']) {
+        delete params.headers['content-length'];
+      }
+
+      for (let key in params.headers) {
+        _headers += `-H '${key}:${params.headers[key]}' `;
+      }
+
+      if (params.body) {
+        for (let key in params.body) {
+          if (params.headers && params.headers['content-type'] && params.headers['content-type'] === 'application/json') {
+            _body += `-d '${JSON.stringify(params.body)}' `;
+          } else {
+            _body += `-d '${key}=${params.body[key]}' `;
+          }
+        }
+      }
+
+      _headers.length ? _headers = _headers.substring(0, _headers.length - 1) : null;
+      _body.length ? _body = _body.substring(0, _body.length - 1) : null;
+
+      var curl = `curl ${_headers} -X ${params.verb.toUpperCase()} '${params.url}' ${_body}`
+      return curl;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const printCurl = function (req, res, next) {
+  var curlParams = {};
+  curlParams.url = req.protocol + '://' + (req.headers.host || req.hostname) + req.originalUrl;
+  curlParams.verb = req.method.toUpperCase();
+  req.headers ? curlParams.headers = req.headers : null;
+  req.body ? curlParams.body = req.body : null;
+  console.log(`${_buildCurl(curlParams)}`);
+
+  next();
+};
+
+module.exports = printCurl;
